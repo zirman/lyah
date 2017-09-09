8567521905bdb8f19e4005ce9f7c99293fb996bf
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2017-09-09 08:46:38 -0400

    [stash]

--- src/Ch15.hs
+++ src/Ch15.hs
@@ -5,7 +5,9 @@ data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 data Direction = L | R deriving (Show)
 type Directions = [Direction]
 
-type Breadcrumbs = [Direction]
+data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
+
+type Breadcrumbs a = [Crumb a]
 
 freeTree :: Tree Char
 freeTree =
@@ -31,6 +33,9 @@ freeTree =
             )
         )
 
+(-:) :: a -> (a -> b) -> b
+x -: f = f x
+
 changeToP :: Directions -> Tree Char -> Tree Char
 changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r
 changeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)
@@ -41,11 +46,63 @@ elemAt (L:ds) (Node _ l _) = elemAt ds l
 elemAt (R:ds) (Node _ _ r) = elemAt ds r
 elemAt [] (Node x _ _) = x
 
-goLeft :: (Tree a, Breadcrumbs) -> (Tree a, Breadcrumbs)
-goLeft (Node _ l _, bs) = (l, L:bs)
+goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goLeft (Node x l r, bs) = (l, LeftCrumb x r : bs)
 
-goRight :: (Tree a, Breadcrumbs) -> (Tree a, Breadcrumbs)
-goRight (Node _ _ r, bs) = (r, R:bs)
+goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goRight (Node x l r, bs) = (r, RightCrumb x l : bs)
 
-(-:) :: a -> (a -> b) -> b
-x -: f = f x
+goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goUp (l, LeftCrumb x r : bs) = (Node x l r, bs)
+goUp (r, RightCrumb x l : bs) = (Node x l r, bs)
+
+type Zipper a = (Tree a, Breadcrumbs a)
+
+--insert :: a -> Zipper a -> Zipper a
+--insert x (Node _ l r, bs) = (Node x l r, bs)
+--insert x (Empty, bs) = (Node x Empty Empty, bs)
+
+modify :: (a -> a) -> Zipper a -> Zipper a
+modify f (Node x l r, bs) = (Node (f x) l r, bs)
+modify f (Empty, bs) = (Empty, bs)
+
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
+
+topMost :: Zipper a -> Zipper a
+topMost (t, []) = (t, [])
+topMost z = topMost (goUp z)
+
+type ListZipper a = ([a], [a])
+
+goForward :: ListZipper a -> ListZipper a
+goForward (x:xs, bs) = (xs, x:bs)
+
+goBackward :: ListZipper a -> ListZipper a
+goBackward (xs, x:bs) = (x:xs, bs)
+
+type Name = String
+type Data = String
+
+data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
+
+myDisk :: FSItem
+myDisk =
+  Folder "root"
+    [ File "goat_yelling_like_a_man.wmv" "baaaaaa"
+    , File "pope_time.avi" "god bless"
+    , Folder "pics"
+      [ File "ape_throwing_up.jpg" "bleargh"
+      , File "watermelon_smash.gif" "smash!!"
+      , File "skull_man(scary).bmp" "Yikes!" ]
+    , File "dijion_poupon.doc" "best mustard"
+    , Folder "programs"
+      [ File "fartwizard.exe" "10gotofart"
+      , File "owl_bandit.dmg" "mov eax, h00t"
+      , File "not_a_virus.exe" "really not a virus"
+      , Folder "source code"
+        [ File "best_hs_prog.hs" "main = print(fix error)"
+        , File "random.hs" "main = print 4" ] ] ]
+
+data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
+type FSZipper = (FSItem, [FSCrumb])
